c44999ae89160ac001332a1185588ccbb555c6d2
bjorn <bjornms@gmail.com>
2012-01-03 20:29:00 +0100

    created the idea of the renderable which is drawn to a rendertarget using a shaderprogram.

--- src/gamearchitecture.cpp
+++ src/gamearchitecture.cpp
@@ -1,4 +1,27 @@
+class ShaderProgram
+{
+public:
+	void use() {};
+};
+
+class Renderable
+{
+public:
+	void draw(const ShaderProgram& sp) {};
+};
+
+class RenderTarget
+{
+public:
+	void use() {};
+};
+
 int main()
 {
+	RenderTarget deferredtarget;
+	Renderable renderable;
+	ShaderProgram shaderprogram;
 
+	deferredtarget.use();
+	renderable.draw(shaderprogram);
 }
