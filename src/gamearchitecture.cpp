a8a9b74615f7073bdf08d631f329076d76039608
bjorn <bjornms@gmail.com>
2012-01-03 21:21:09 +0100

    added object abstraction which has a position and orientation

--- src/gamearchitecture.cpp
+++ src/gamearchitecture.cpp
@@ -1,3 +1,13 @@
+struct vec3
+{
+	float x, y, z;
+};
+
+struct quat
+{
+	float x, y, z, w;
+};
+
 class ShaderProgram
 {
 public:
@@ -16,12 +26,36 @@ public:
 	void use() {};
 };
 
+class Object
+{
+private:
+	Renderable& renderable;
+	vec3& position;
+	quat& orientation;
+public:
+	Object(Renderable& renderable, vec3 position, quat orientation)
+	: renderable(renderable)
+	, position(position)
+	, orientation(orientation)
+	{
+	};
+	void draw(const ShaderProgram& sp)
+	{
+		// push postion and orientation as worldmatrix to gpu so it can do translation and orientation.
+		renderable.draw(sp);
+	}
+};
+
 int main()
 {
 	RenderTarget deferredtarget;
 	Renderable renderable;
+	vec3 pos;
+	quat q;
+	Object object(renderable, pos, q);
 	ShaderProgram shaderprogram;
 
 	deferredtarget.use();
-	renderable.draw(shaderprogram);
+	object.draw(shaderprogram);
 }
+
