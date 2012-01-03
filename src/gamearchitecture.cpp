e4f27f35d997f30c775a601d72920b81eb113f9a
bjorn <bjornms@gmail.com>
2012-01-03 21:51:44 +0100

    Made plane as a special case of object

--- src/gamearchitecture.cpp
+++ src/gamearchitecture.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+
 struct vec3
 {
 	float x, y, z;
@@ -11,38 +13,64 @@ struct quat
 class ShaderProgram
 {
 public:
-	void use() {};
+	void use() {}
+	void set(const vec3& position, const quat& orientation) const {}
 };
 
 class Renderable
 {
 public:
-	void draw(const ShaderProgram& sp) {};
+	virtual void draw(const ShaderProgram& sp) {}
 };
 
 class RenderTarget
 {
 public:
-	void use() {};
+	void use() {}
 };
 
-class Object
+class Object : public Renderable
 {
 private:
 	Renderable& renderable;
 	vec3& position;
 	quat& orientation;
 public:
-	Object(Renderable& renderable, vec3 position, quat orientation)
+	Object(Renderable& renderable, vec3& position, quat& orientation)
 	: renderable(renderable)
 	, position(position)
 	, orientation(orientation)
 	{
-	};
+	}
+	void update()
+	{
+		// do something with position and orientation
+		position.x += 1;
+	}
 	void draw(const ShaderProgram& sp)
 	{
-		// push postion and orientation as worldmatrix to gpu so it can do translation and orientation.
+		sp.set(position, orientation);
 		renderable.draw(sp);
+	}
+};
+
+class Plane : public Renderable
+{
+private:
+	Object object;
+	Renderable renderable;
+	vec3 pos;
+	quat q;
+public:
+	Plane(size_t size)
+	// construct object with renderable which holds a Plane
+	: object(renderable, pos, q)
+	{
+		
+	}
+	void draw(const ShaderProgram& sp)
+	{
+		object.draw(sp);
 	}
 };
 
@@ -53,9 +81,11 @@ int main()
 	vec3 pos;
 	quat q;
 	Object object(renderable, pos, q);
+	Plane p(3);
 	ShaderProgram shaderprogram;
 
 	deferredtarget.use();
 	object.draw(shaderprogram);
+	p.draw(shaderprogram);
 }
 
