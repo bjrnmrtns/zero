dda7af37bbb12d1648104722681aad92a6583af3
bjorn <bjornms@gmail.com>
2012-02-22 21:32:25 +0100

    .

--- glfwtest.cpp
+++ glfwtest.cpp
@@ -462,17 +462,23 @@ public:
 		glm::vec3 normal;
 		glm::vec2 texcoord;
 	};
+private:
+	//VertexBuffer vb;
 public:
 	static const VertexBuffer::InputElementDescription description[];
+	Model(VertexBuffer::InputElementDescription description[], Vertex vertices[], size_t size)
+	: vb(description, vertices, size)
+	{
+	}
 	static Model& square()
 	{
-		static const Vertex vertices[] { { glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(0,  0) },
-		                                 { glm::vec3(-1,  1, 1), glm::vec3(0, 0, 1), glm::vec2(0,  1) },
-		                                 { glm::vec3( 1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(1,  0) },
-		                                 { glm::vec3(-1,  1, 1), glm::vec3(0, 0,-1), glm::vec2(0,  1) },
-		                                 { glm::vec3( 1,  1, 1), glm::vec3(0, 0,-1), glm::vec2(1,  1) },
-		                                 { glm::vec3( 1, -1, 1), glm::vec3(0, 0,-1), glm::vec2(1,  0) } };
-		static Model model;
+		static const Vertex vertices[] { { glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(0, 0) },
+		                                 { glm::vec3(-1,  1, 1), glm::vec3(0, 0, 1), glm::vec2(0, 1) },
+		                                 { glm::vec3( 1, -1, 1), glm::vec3(0, 0, 1), glm::vec2(1, 0) },
+		                                 { glm::vec3(-1,  1, 1), glm::vec3(0, 0,-1), glm::vec2(0, 1) },
+		                                 { glm::vec3( 1,  1, 1), glm::vec3(0, 0,-1), glm::vec2(1, 1) },
+		                                 { glm::vec3( 1, -1, 1), glm::vec3(0, 0,-1), glm::vec2(1, 0) } };
+		static Model model(description, vertices, sizeof(vertices)/sizeof(float));
 		return model;
 	};
 };
