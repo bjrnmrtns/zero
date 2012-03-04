51bab10f5805552f09c074633c50235813ed9fe3
Bjorn <bjornms@gmail.com>
2012-03-04 18:43:48 +0100

    framework for webserver

--- webserver.cpp
+++ webserver.cpp
@@ -8,7 +8,9 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <thread>
+#include <vector>
 #include <iostream>
+#include <map>
 
 class GeneralException : public std::exception
 {
@@ -28,12 +30,14 @@ private:
 	const std::string errors;
 };
 
-class ClientSocket
+namespace http
+{
+class socket
 {
 private:
 	int fd;
 public:
-	ClientSocket(int fd)
+	socket(int fd)
 	: fd(fd)
 	{
 	}
@@ -45,20 +49,20 @@ public:
 	{
 		return ::write(fd, buffer, count);
 	}
-	~ClientSocket()
+	~socket()
 	{
 		close(fd);
 	}
 };
 
-class ServerSocket
+class serversocket
 {
 private:
 	int fd;
 public:
-	ServerSocket(unsigned short port)
+	serversocket(unsigned short port)
 	{
-		fd = socket(AF_INET, SOCK_STREAM, 0);
+		fd = ::socket(AF_INET, SOCK_STREAM, 0);
 		if(fd < 0)
 		{
 			std::string message("Failed to create socket: ");
@@ -82,34 +86,34 @@ public:
 			throw GeneralException(message + error);
 		}
 	}
-	~ServerSocket()
+	~serversocket()
 	{
 		close(fd);
 	}
-	ClientSocket* Accept(unsigned short &port, std::string &ipaddress)
+	socket* accept(unsigned short &port, std::string &ipaddress)
 	{
 		struct sockaddr_in addr;
 		unsigned int addrlen;
-		int newfd = accept(fd, (struct sockaddr *)&addr, &addrlen);
+		int newfd = ::accept(fd, (struct sockaddr *)&addr, &addrlen);
 		if(newfd <= 0) throw GeneralException("No socket accepted");
 		port = ntohs(addr.sin_port);
 		char ipaddr[16];
 		char *raw = (char *)&addr.sin_addr.s_addr;
 		sprintf(ipaddr, "%d.%d.%d.%d", raw[0], raw[1], raw[2], raw[3]);
 		ipaddress = ipaddr;
-		return new ClientSocket(newfd);
+		return new socket(newfd);
 	}
 };
 
-class HttpRequest
+class request
 {
 private:
-	ClientSocket *cs;
-	std::thread t;
+	socket& cs;
+	std::string method, url, httpversion;
+	std::map<std::string, std::string> attributes, arguments;
 public:
-	HttpRequest(ClientSocket *cs)
+	request(socket& cs)
 	: cs(cs)
-	, t(std::ref(*this))
 	{
 	}
 	std::string readline()
@@ -117,52 +121,134 @@ public:
 		char buffer[1024];
 		int idx = 0;
 		do {
-			cs->read(&buffer[idx++], 1);
-		} while(idx < 2 || buffer[idx-2] != '\r' && buffer[idx-1] != '\n');
+			cs.read(&buffer[idx++], 1);
+		} while(idx < 2 || (buffer[idx-2] != '\r' && buffer[idx-1] != '\n'));
 		return std::string(buffer, 0, idx-2);
 	}
 	void operator()()
 	{
-		std::string argline = readline();
-		std::cout << argline << std::endl;
-		while(argline.size() > 0)
+		std::string requestLine = readline();
+		int firstSpace = requestLine.find(" ", 0);
+		int secondSpace = requestLine.find(" ", firstSpace+1);
+		method = requestLine.substr(0, firstSpace);
+		url = requestLine.substr(firstSpace+1, secondSpace - firstSpace - 1);
+		httpversion = requestLine.substr(secondSpace+1);
+		std::string argLine = readline();
+		while (argLine.size())
 		{
-			argline = readline();
-			std::cout << argline << std::endl;
+			std::string name, arg;
+			int separator = argLine.find(": ", 0);
+			name = argLine.substr(0, separator);
+			arg = argLine.substr(separator+2);
+			attributes.insert(std::make_pair(name, arg));
+			argLine = readline();
 		}
+		if (url.find_first_of('?') != std::string::npos)
+		{
+			std::string args = url.substr(url.find_first_of('?')+1);
+			url = url.substr(0, url.find_first_of('?'));
+			while (args.size() > 0)
+			{
+				size_t nextAmp = args.find_first_of('&');
+				std::string nextArg;
+				if (nextAmp == std::string::npos)
+				{
+					nextArg = args;
+					args = "";
+				} 
+				else
+				{
+					nextArg = args.substr(0, nextAmp);
+					args = args.substr(nextAmp+1);
+				}
+				std::string val = "true";
+				size_t eq = nextArg.find_first_of('=');
+				if (eq != std::string::npos)
+				{
+					val = nextArg.substr(eq+1);
+					nextArg = nextArg.substr(0, eq);
+				}
+				arguments.insert(std::make_pair(nextArg, val));
+			}
+		}
+		if (url[url.size()-1] == '/') url = url.substr(0, url.size()-1);
 	}
 };
 
-class Webserver
+class response
 {
-ServerSocket server;
-bool running;
+private:
+	socket& sock;
 public:
-	static Webserver &Instance()
-	{ 
-		static Webserver webserver(1080); 
-		return webserver; 
-	}
-	Webserver(int port)
-	: server(port)
-	, running(true)
+	response(socket& sock)
+	: sock(sock)
 	{
 	}
 	void operator()()
 	{
-		while(running)
+	}
+};
+
+class server
+{
+public:
+	class handler
+	{
+	public:
+		virtual response handle(socket *sock) = 0;
+	};
+private:
+	serversocket ssock;
+	std::vector<std::pair<std::string, server::handler*>> uris;
+public:
+	static server &Instance()
+	{ 
+		static server server_(1080); 
+		return server_;
+	}
+	server(int port)
+	: ssock(port)
+	{
+	}
+	void operator()()
+	{
+		for(;;)
 		{
 			unsigned short port;
 			std::string address;
-			ClientSocket *client = server.Accept(port, address);
-			HttpRequest *request = new HttpRequest(client);
+			std::thread t(server::handle, ssock.accept(port, address));
+			t.detach();
 		}
+	}
+	static void handle(socket *clientsock)
+	{
+		std::unique_ptr<socket> cs(clientsock);
+		request req(*cs.get());
+		response resp(*cs.get());
+		req();
+		resp();
+	}
+	void registeruri(std::string uri, server::handler *handler)
+	{
+		uris.push_back(std::make_pair(uri, handler));
+	}
+};
+}
+
+class TextureHandler : public http::server::handler
+{
+public:
+	http::response handle(http::socket *sock)
+	{
+		http::response resp(*sock);
+		return resp;
 	}
 };
 
 int main()
 {
-	std::thread t(std::ref(Webserver::Instance()));
-	t.join();
+	TextureHandler texhandler;
+	http::server::Instance().registeruri("textures", &texhandler);
+	http::server::Instance()();
 	return 0;
 }
